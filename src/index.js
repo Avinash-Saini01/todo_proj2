8be53c71b807cfa5980610bfef2dbab00e32d254
Avinash-Saini01 <87408580+Avinash-Saini01@users.noreply.github.com>
2024-07-09 00:39:39 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// The code that was originally there
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// The code we'll add
+import store from "./redux/config/configStore";
+import { Provider } from "react-redux";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+
+	//App as a Provider, and put in the store that was exported by default from configStore.
+  <Provider store={store}>
+    <App />.
+  </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: <https://bit.ly/CRA-vitals>
+reportWebVitals();
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
